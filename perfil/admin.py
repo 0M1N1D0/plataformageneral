4017a9f2eaffd7c5a9eeb5b7a273f31b29e213fe
Miguel Angel Alvarez <maagfis@gmail.com>
2023-05-04 16:26:14 -0600

    .

--- perfil/admin.py
+++ perfil/admin.py
@@ -1,18 +1,6 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+# from django.contrib import admin
+# from django.contrib.auth.admin import UserAdmin
 
-from .models import CustomUser
+# from .models import CustomUser
 
-# class CustomUserAdmin(UserAdmin):
-#     list_display = (
-#         'username', 
-#         'nombres', 
-#         'apellido_paterno', 
-#         'apellido_materno', 
-#         'numero_nomina', 
-#         'email', 
-#         'supervisor', 
-#         'is_admin'
-#     )
-
-admin.site.register(CustomUser, UserAdmin)
+# admin.site.register(CustomUser, UserAdmin)
